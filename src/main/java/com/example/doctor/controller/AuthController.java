c89df370488ae43b1450989ca14aadfb76777806
hien <ngochien15092000@gmail.com>
2021-12-09 13:50:25 +0700

    update doctor

--- src/main/java/com/example/doctor/controller/AuthController.java
+++ src/main/java/com/example/doctor/controller/AuthController.java
@@ -12,18 +12,19 @@ import com.example.doctor.service.AccountService;
 import com.example.doctor.service.DoctorService;
 import com.example.doctor.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.util.List;
-
-
 @Controller
 @RequestMapping("/")
 public class AuthController {
+
     @Autowired
     private DoctorService doctorService;
 
